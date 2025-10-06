f8aa7b0b77c28ba83edb9a36c444afd06541fd46
lexi-jones <jonesae@mit.edu>
2025-10-06 11:04:17 -0400

    fixed compatability with GLOBCURRENT

--- physics_pipeline/gaussian_lag_sims_v4.py
+++ physics_pipeline/gaussian_lag_sims_v4.py
@@ -5,14 +5,14 @@
 
 # Lexi Jones-Kellett
 # Date created: 10/21/24
-# Last edited: 08/12/25
+# Last edited: 10/06/25
 
 import csv,os,math,random,sys,cftime
 import numpy as np
 import xarray as xr
 from glob import glob
 from datetime import timedelta,datetime
-from parcels import FieldSet, ParticleSet, Variable, JITParticle, AdvectionRK4, ErrorCode,Field
+from parcels import FieldSet, ParticleSet, Variable, JITParticle, AdvectionRK4, ErrorCode, Field
 from config import *
 
 #input_dataset = str(sys.argv[1]) # options: 'cmems','oscar','globcurrent0','globcurrent15'
@@ -22,7 +22,7 @@ print("Packages loaded...")
 
 metadata = []
 metadata_filename = data_dir + 'G4_metadata_no_physics.csv'
-with open(metadata_dir + metadata_filename) as csv_file:
+with open(metadata_filename) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
 #        if str(row[0]) == '60': # do this if you want to run a single site
@@ -61,7 +61,6 @@ runtime = 200 # days
 timestep_mins = 20 # minutes
 output_freq = 6 #hours
 
-#for input_dataset in ['cmems','oscar_interim','globcurrent0']:
 for input_dataset in ['cmems','oscar_final','globcurrent15']:
 
     if input_dataset == 'cmems':
@@ -88,39 +87,58 @@ for input_dataset in ['cmems','oscar_final','globcurrent15']:
                       'V': {'lon':'lon','lat':'lat','time':'time'}}    
         output_dir = data_dir + 'parcels_trajs/gaussian_seeding_sample_sites/OSCAR_final_runs/'
         
-    elif (input_dataset == 'globcurrent0') or (input_dataset == 'globcurrent15'):
+    elif (input_dataset == 'globcurrent0'):
         parcels_input_files = sorted(glob(data_dir + 'GLOBCURRENT/cmems_obs_mob_glo_phy-cur_my*.nc'))
         filenames = {'U': parcels_input_files,'V': parcels_input_files}
-        variables = {'U': 'ugos','V': 'vgos'}
-        dimensions = {'U': {'lon':'longitude','lat':'latitude','time':'time'},
-                      'V': {'lon':'longitude','lat':'latitude','time':'time'}}
-        output_dir = data_dir + 'parcels_trajs/gaussian_seeding_sample_sites/GC_runs/'
-
+        variables = {'U': 'uo','V': 'vo'} #uo is Ekman+geos field, whereas ugos is geos only
+        dimensions = {'U': {'lon':'longitude','lat':'latitude','time':'time','depth':'depth'},
+                      'V': {'lon':'longitude','lat':'latitude','time':'time','depth':'depth'}}
+        indeces = {'depth':[0]}
+        output_dir = data_dir + 'parcels_trajs/gaussian_seeding_sample_sites/GC0_runs/'
+    
+    elif (input_dataset == 'globcurrent15'):
+        parcels_input_files = sorted(glob(data_dir + 'GLOBCURRENT/cmems_obs_mob_glo_phy-cur_my*.nc'))
+        filenames = {'U': parcels_input_files,'V': parcels_input_files}
+        variables = {'U': 'uo','V': 'vo'} #uo is Ekman+geos field, whereas ugos is geos only
+        dimensions = {'U': {'lon':'longitude','lat':'latitude','time':'time','depth':'depth'},
+                      'V': {'lon':'longitude','lat':'latitude','time':'time','depth':'depth'}}
+        indeces = {'depth':[1]} # sets field to next level for depth
+        output_dir = data_dir + 'parcels_trajs/gaussian_seeding_sample_sites/GC15_runs/'
+        
+    # include depth indices if running in Globcurrent data 
+    if (input_dataset == 'globcurrent0') or (input_dataset == 'globcurrent15'):
+        fieldset = FieldSet.from_netcdf(filenames, variables, dimensions, indeces, allow_time_extrapolation=True) 
+    else:
+        fieldset = FieldSet.from_netcdf(filenames, variables, dimensions, allow_time_extrapolation=True) 
+        
     print("Advecting particles for sample site...")
 
     for m in metadata[1:]:
     #for m in metadata:
         print(m[0])
 
-        fieldset = FieldSet.from_netcdf(filenames, variables, dimensions, allow_time_extrapolation=True) 
-
         sample_date,sample_time = m[1],m[2]
 
-        # CMEMS & GLORYS use np.datetime, and OSCAR uses cftime
+        # CMEMS & GLOBCURRENT use np.datetime, and OSCAR uses cftime
         datetime_time = datetime(int(sample_date[0:4]),int(sample_date[4:6]),int(sample_date[6:8]),int(sample_time[0:2]),int(sample_time[3:5]))
         cftime_time = cftime.DatetimeJulian(int(sample_date[0:4]),int(sample_date[4:6]),int(sample_date[6:8]),int(sample_time[0:2]),int(sample_time[3:5]))
 
         lon_list = (float(m[4])+points[:,0]).tolist()
         lat_list = (float(m[3])+points[:,1]).tolist()
 
-        # CMEMS & OSCAR are 0-360 lon, GLORYS is -180 to 180
-        #if input_dataset == 'glorys':
-        #    lon_list = [i+360 for i in lon_list] #only works for negative lons
-
+        # CMEMS & OSCAR are 0-360 lon, GLOBCURRENT is -180 to 180
+        if (input_dataset == 'globcurrent0') or (input_dataset == 'globcurrent15'):
+            lon_list = [i-360 for i in lon_list] #only works for negative lons
+            
+        # Set up particle set
         if (input_dataset == 'oscar_interim') or (input_dataset == 'oscar_final'):
             pset = ParticleSet.from_list(fieldset = fieldset, pclass=JITParticle, 
                                          lon = lon_list, lat = lat_list, 
                                          depth = [0]*len(lon_list), time = [cftime_time]*len(lon_list))
+        elif (input_dataset == 'globcurrent15'): # set depth to 15m
+            pset = ParticleSet.from_list(fieldset = fieldset, pclass=JITParticle, 
+                                         lon = lon_list, lat = lat_list, 
+                                         depth = [15]*len(lon_list), time = [datetime_time]*len(lon_list))
         else:
             pset = ParticleSet.from_list(fieldset = fieldset, pclass=JITParticle, 
                                          lon = lon_list, lat = lat_list, 
